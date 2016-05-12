f95b4d842ed0240f3997f7c76d03f167f9750165
Gary Ewan Park <gep13@gep13.co.uk>
2016-05-12 19:40:49 +0100

    (GH-10) Added cake.config download command

--- src/cakeMain.ts
+++ src/cakeMain.ts
@@ -2,11 +2,16 @@
 
 import { commands, ExtensionContext } from 'vscode';
 import { installCakeBootstrapper } from './bootstrapper/cakeBootstrapperCommand';
+import { installCakeConfiguration } from './configuration/cakeConfigurationCommand';
 
 export function activate(context: ExtensionContext): void {
     // Register the bootstrapper command.
     context.subscriptions.push(commands.registerCommand('cake.bootstrapper', async () => {
         installCakeBootstrapper();
+    }));
+    // Register the configuration command.
+    context.subscriptions.push(commands.registerCommand('cake.configuration', async () => {
+        installCakeConfiguration();
     }));
 }
 
