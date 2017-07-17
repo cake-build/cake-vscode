588f7ff78ccd82a416cba48e3f05603fc88fef7a
Gary Ewan Park <gep13@gep13.co.uk>
2017-07-17 19:53:27 +0100

    (GH-14) Added initial implementation
    - Based on feedback from SO questions:
    https://stackoverflow.com/questions/45111482
    https://stackoverflow.com/questions/45090999
    - As well as Twitter conversation:
    https://twitter.com/ErichGamma/status/885822561136803840

--- src/cakeMain.ts
+++ src/cakeMain.ts
@@ -3,6 +3,11 @@
 import * as vscode from 'vscode';
 import { installCakeBootstrapper } from './bootstrapper/cakeBootstrapperCommand';
 import { installCakeConfiguration } from './configuration/cakeConfigurationCommand';
+import * as fs from 'fs';
+import * as os from 'os';
+
+type AutoDetect = 'on' | 'off';
+let taskProvider: vscode.Disposable | undefined;
 
 export function activate(context: vscode.ExtensionContext): void {
     // Register the bootstrapper command.
@@ -13,7 +18,84 @@ export function activate(context: vscode.ExtensionContext): void {
     context.subscriptions.push(vscode.commands.registerCommand('cake.configuration', async () => {
         installCakeConfiguration();
     }));
+
+    function onConfigurationChanged() {
+        let autoDetect = vscode.workspace.getConfiguration('npm').get<AutoDetect>('autoDetect');
+		if (taskProvider  && autoDetect === 'off') {
+            taskProvider.dispose();
+            taskProvider = undefined;
+        } else if (!taskProvider && autoDetect === 'on') {
+            taskProvider = vscode.workspace.registerTaskProvider('cake', {
+                provideTasks: async () => {
+                    return await getCakeScriptsAsTasks();
+                },
+                resolveTask(_task: vscode.Task): vscode.Task | undefined {
+                    return undefined;
+                }
+            });
+        }
+    }
+
+    vscode.workspace.onDidChangeConfiguration(onConfigurationChanged);
+    onConfigurationChanged();
 }
 
 export function deactivate() {
+    if (taskProvider) {
+        taskProvider.dispose();
+    }
 }
+
+interface CakeTaskDefinition extends vscode.TaskDefinition {
+    script: string;
+    file?: string;
+}
+
+async function getCakeScriptsAsTasks(): Promise<vscode.Task[]> {
+    let workspaceRoot = vscode.workspace.rootPath;
+    let emptyTasks: vscode.Task[] = [];
+
+    if (!workspaceRoot) {
+        return emptyTasks;
+    }
+
+    try {
+        let files = await vscode.workspace.findFiles('**/*.cake', 'tools');
+        if (files.length === 0) {
+            return emptyTasks;
+        }
+        const result: vscode.Task[] = [];
+
+        files.forEach(file => {
+            const contents = fs.readFileSync(file.fsPath).toString();
+
+            let taskRegularExpression = /Task\("(.*?)"\)/g;
+
+            let matches, taskNames = [];
+            while (matches = taskRegularExpression.exec(contents)) {
+                taskNames.push(matches[1]);
+            }
+
+            taskNames.forEach(taskName => {
+                const kind: CakeTaskDefinition = {
+                    type: 'cake',
+                    script: taskName
+                };
+
+                let buildCommand = `./build.sh --target \"${taskName}\"`;
+                if (os.platform() === "win32") {
+                    buildCommand = `powershell -ExecutionPolicy ByPass -File build.ps1 -target \"${taskName}\"`;
+                }
+
+                const buildTask = new vscode.Task(kind, `Run ${taskName}`, 'Cake', new vscode.ShellExecution(`${buildCommand}`));
+                buildTask.group = vscode.TaskGroup.Build;
+
+                result.push(buildTask);
+            });
+        });
+
+        return result;
+    } catch (e) {
+        return [];
+    }
+};
