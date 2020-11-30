2b560db69195098c764c4eab261a0d2a402b54eb
Gary Ewan Park <gep13@gep13.co.uk>
2020-11-30 12:34:53 -0800

    (maint) Update enum string values
    As discussed on stream, lets be a little bit more specific about the
    names for the enums, so that it is clearer about what is being
    installed.

--- src/shared/enums.ts
+++ src/shared/enums.ts
@@ -1,10 +1,10 @@
 export enum RunnerType {
-  NetTool = "NETTOOL",
-  NetFramwork = "NETFRAMEWORK",
-  NetCore = "NETCORE"
+  NetTool = "Cake .NET Tool (Global)",
+  NetFramwork = "Cake runner for .NET Framework",
+  NetCore = "Cake runner for .NET Core"
 }
 
 export enum DebugType {
-  NetTool = "NETTOOL",
-  NetCore = "NETCORE"
+  NetTool = "Cake .NET Tool (Global)",
+  NetCore = "Cake runner for .NET Core"
 }
